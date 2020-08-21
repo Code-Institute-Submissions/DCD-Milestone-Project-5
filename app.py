18c5f9acab61fe4b9fc1018249c19fdd45c50fde
PokeyMcFork <pokeymcfork@gmail.com>
2020-08-21 00:20:42 +0000

    addition of run section of app

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+# imports
 import os
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
+# creating flask instance
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)  # remove debug for release version
