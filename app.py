8e074e46c77979648b4b5782be151439c04eb31b
PokeyMcFork <pokeymcfork@gmail.com>
2020-09-08 01:27:47 +0000

    added edit & insert routes, base template styling, Jinja recipe stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # imports
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -23,13 +23,27 @@ mongo = PyMongo(app)
 def home():
     return render_template("home.html", recipes_popular=mongo.db.recipes.find(), recipes_new=mongo.db.recipes.find())
 
+
 @app.route('/recipes')
 def recipe_search():
     return render_template("recipe_list.html", recipes=mongo.db.recipes.find())
 
+
 @app.route('/recipes/<ID>')
 def recipe_page(ID):
-    return render_template("recipe_page.html", )
+    return render_template("recipe_page.html")
+
+
+@app.route('/edit_recipe')
+def edit_recipe():
+    return render_template("edit_recipe.html")
+
+
+@app.route('/insert_recipe', methods=["GET", "POST"])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('recipe_search'))
 
 
 if __name__ == '__main__':
