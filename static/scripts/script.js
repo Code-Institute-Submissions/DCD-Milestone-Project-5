10dac3c53b087eb30283e0de7024cbd89e11ae10
PokeyMcFork <pokeymcfork@gmail.com>
2020-10-09 02:41:09 +0000

    further tinyMCE integration, ready for styling and validation.

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -12,7 +12,24 @@ function searchPaginate(){
 
 //TinyMCE integration
 tinymce.init({
-    selector:'textarea#cookingMethod'
+    selector:'.tiny-mce-target-bul',
+    plugins:'lists',
+    toolbar:'bullist',
+    valid_elements:'p,ul,ol,li',
+    menubar:false,
+    element_format:'html',
+    lists_indent_on_tab:false
+});
+
+
+tinymce.init({
+    selector:'.tiny-mce-target-num',
+    plugins:'lists',
+    toolbar:'numlist',
+    valid_elements:'p,ul,ol,li',
+    menubar:false,
+    element_format:'html',
+    lists_indent_on_tab:false
 });
 
 $(document).on('focusin', function(e) {
