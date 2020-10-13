666d09f71acbd0c896d00ec6a9c05821e8d80e7f
PokeyMcFork <pokeymcfork@gmail.com>
2020-10-13 02:37:04 +0000

    implemented full search pagination

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,73 +1,78 @@
+/*jshint esversion: 6 */
+
 /* searchMenuToggle()
 
 This function is called when clicking the menu toggle button on the search page, which is only visible on small viewports.
 it toggles the visibility of the search menu.
 
 */
-function searchMenuToggle(){
-    let target = document.getElementById("recipe-search-controls");
-    displayType = getComputedStyle(target).display;
-    if(displayType != "none"){
-        target.style.display = "none";
-    }
-    else{
+function searchMenuToggle() {
+	let target = document.getElementById("recipe-search-controls");
+	let displayType = getComputedStyle(target).display;
+	if (displayType != "none") {
+		target.style.display = "none";
+	} else {
         target.style.display = "flex";
-    }
+	}
 }
 
 /*
 searchPaginate()
 
-This function uses Ajax calls to perform dynamic pagination
-of the serach results, to allow for a seamless browsing experience
-
 */
-function searchPaginate(totalPages, page){
+function searchPaginate(totalPages, currentPage, numResults) {
     //first loop that changes which page is currently active.
-    for(let a = 1; a <= totalPages; a++)
-    {
-        let elementID = `page-button-${a}`;
-        if(a = page){
-            document.getElementById(elementID).classList.add('active');
+	for (a = 1; a <= totalPages; a++) {
+		var elementID = `page-button-${a}`;
+		if (a === currentPage) {
+			document.getElementById(elementID).classList.add('active');
+		} else {
+			document.getElementById(elementID).classList.remove('active');
+        }
+    }
+    
+    let upperBounds = (8 * currentPage);
+    let lowerBounds = (upperBounds) -7;
+    console.log(upperBounds);
+    console.log(lowerBounds);
+	for (b = 1; b <= numResults; b++) {
+		let recipeTarget = document.getElementById(`recipe-${b}`);
+        if (b>=lowerBounds && b<=upperBounds){
+            recipeTarget.style.display = 'inline-flex';
         }
         else{
-            document.getElementById(elementID).classList.remove('active');
+            recipeTarget.style.display = 'none';
         }
-    }
-    for(let b = 1; b<=totalPages*8; b++){
-        recipeTarget = document.getElementById(`recipe-${b}`)
-        if(recipeTarget != null && (b>(8*page-7) && b<(8*page)))
-        {
-            getComputedStyle(recipeTarget).display='none';
-        }
-    }
+        
+	}
 }
 
 
-//TinyMCE integration
+//tinyMCE integration
 tinymce.init({
-    selector:'.tiny-mce-target-bul',
-    plugins:'lists',
-    toolbar:'bullist',
-    valid_elements:'p,ul,ol,li',
-    menubar:false,
-    element_format:'html',
-    lists_indent_on_tab:false
+	selector: '.tiny-mce-target-bul',
+	plugins: 'lists',
+	toolbar: 'bullist',
+	valid_elements: 'p,ul,ol,li',
+	menubar: false,
+	element_format: 'html',
+	lists_indent_on_tab: false
 });
 
 
 tinymce.init({
-    selector:'.tiny-mce-target-num',
-    plugins:'lists',
-    toolbar:'numlist',
-    valid_elements:'p,ul,ol,li',
-    menubar:false,
-    element_format:'html',
-    lists_indent_on_tab:false
+	selector: '.tiny-mce-target-num',
+	plugins: 'lists',
+	toolbar: 'numlist',
+	valid_elements: 'p,ul,ol,li',
+	menubar: false,
+	element_format: 'html',
+	lists_indent_on_tab: false
 });
 
+//tinyMCE Bootstrap cross-integration
 $(document).on('focusin', function(e) {
-  if ($(e.target).closest(".tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
-    e.stopImmediatePropagation();
-  }
+	if ($(e.target).closest(".tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
+		e.stopImmediatePropagation();
+	}
 });
